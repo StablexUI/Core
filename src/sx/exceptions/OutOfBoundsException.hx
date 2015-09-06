51e9176a9672fa9025fb1b7bbe9a6b94acd1a4c1
Alexander Kuzmenko <alex@stablex.ru>
2015-09-07 01:26:13 +0300

    minor

---
+++ src/sx/exceptions/OutOfBoundsException.hx
@@ -0,0 +1,23 @@
+package sx.exceptions;
+
+import haxe.PosInfos;
+
+
+
+/**
+ * Provided argument does not fit required bounds.
+ *
+ */
+class OutOfBoundsException extends SxException
+{
+
+    /**
+     * Cosntructor
+     *
+     */
+    public function new (msg:String = 'Provided argument does not fit required bounds.', pos:PosInfos = null) : Void
+    {
+        super(msg, pos);
+    }
+
+}//class OutOfBoundsException
