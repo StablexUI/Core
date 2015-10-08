b910b72dfe6ffd528c2a701aca080eee1924ee7b
Alexander Kuzmenko <alex@stablex.ru>
2015-10-08 16:30:53 +0300

    touchId for PointerSignal

--- src/sx/signals/PointerSignal.hx
+++ src/sx/signals/PointerSignal.hx
@@ -10,6 +10,6 @@ import sx.widgets.Widget;
  * @param   Widget  Widget which is currently processing signal.
  * @param   Widget  Widget which initiated this signal.
  * @param   Int     A unique identifier assigned to the touch point (for touch events).
- *                  For mouse events these identifiers are negative.
+ *                  For mouse events these identifiers are negative if not provided by current backend.
  */
 typedef PointerSignal = Signal<Widget->Widget->Int->Void>;
