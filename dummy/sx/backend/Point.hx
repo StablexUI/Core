04be68510c71ed5291a88a2e6b0d0811507730b5
Alexander Kuzmenko <alex@stablex.ru>
2015-12-13 00:33:38 +0300

    mor informative comment for Point class

--- dummy/sx/backend/Point.hx
+++ dummy/sx/backend/Point.hx
@@ -3,6 +3,23 @@ package sx.backend;
 
 /**
  * Should be replaced with native backend Point class.
- * Point is expected to have decimal `x` and `y` properties measured in pixels.
+ * Point is expected to have decimal `x` and `y` properties measured in pixels and a constructor accepting `x` and `y`.
  */
 typedef Point = sx.backend.dummy.Point;
+
+
+/**
+This is minimal required Point implementation:
+
+class Point
+{
+    public var x : Float = 0;
+    public var y : Float = 0;
+
+    public function new (x:Float = 0, y:Float = 0) : Void
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+*/
